4265496a7935c8a25e1ee76e27f922529bf3c790
patriciapastorpuente <patripastorpuente@gmail.com>
2024-05-20 19:38:22 +0200

    Seguridad

--- public/assets/keys.js
+++ public/assets/keys.js
@@ -1,12 +1,4 @@
-// Importante guardar en documentos aparte las claves secretas para mejor seguridad
-// copiamos las claves de nuestra API
 export default {
     public: "pk_test_51P4k6xBreChkiAzS3KhGGopFsjxvrmWOWhVmGZMBgI0sGTrEe6omErqC8MwnhcbWrmmIL7SZPEVaCi4xiRPJaR7j00aWk9DiL9",
     secret: "sk_test_51P4k6xBreChkiAzSIRrD1dZ9kOTNEVbA6QqFREf34onuwRlMlAJcgKwF2rPWx9u4QNh63enYKPkPLwYNcN3UKqkI007579kekr"
 }
-
-
-// CLAVES DE LOS PRODUCTOS Y PRECIOS ACTUALES SEGÚN STRIPE
-// PRESENCIAL       price_1P6LlFBreChkiAzSPWqgWF52
-// TELEFÓNICA       price_1P54YJBreChkiAzS2SWlAhY2
-// VIDEOCONFERENCIA price_1P54X0BreChkiAzSA5m0kb2m
